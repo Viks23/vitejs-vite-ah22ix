70025e91b6e98b6b1f41cb4bf3ecb9995818aac1
Viks23 <vrai07008@gmail.com>
2024-12-01 17:08:38 +0530

    V1.0.0.0.5

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -7,17 +7,12 @@ function ListGroup() {
     'Paris'
   ];
   items = [];
-  if (items.length === 0)
-    return (
-      <>
-        <h1>List</h1>
-        <p>No item found</p>
-      </>
-    );
 
+  const message = items.length === 0 && <p>No item found</p>;
   return (
     <>
       <h1>List </h1>
+      {message}
       <ul className="list-group">
         {items.map(item =>
           <li className="list-group-item" key={item}>{item}</li>)}

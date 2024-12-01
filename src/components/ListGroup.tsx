c0ec335416c412e8feccbd4ad362550d58f2904f
Viks23 <vrai07008@gmail.com>
2024-12-01 19:51:23 +0530

    V1.0.0.1.0

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,31 +1,34 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
-  let items = [
-    'New York',
-    'San Frantcisco',
-    'Tokyo',
-    'London',
-    'Paris'
-  ];
+  let items = ["New York", "San Frantcisco", "Tokyo", "London", "Paris"];
 
-  // Event handler
-  const handleClick = (event: MouseEvent) => console.log(event);
+  //Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List </h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map(item =>
-          <li className="list-group-item" key={item} onClick={handleClick}>{item}</li>)}
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
 }
 
 export default ListGroup;
-
-
-
-

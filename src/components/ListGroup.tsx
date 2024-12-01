a933970c6807a31363f66dd738264c3fd775b91a
Viks23 <vrai07008@gmail.com>
2024-12-01 17:10:12 +0530

    V1.0.0.0.6

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -8,11 +8,10 @@ function ListGroup() {
   ];
   items = [];
 
-  const message = items.length === 0 && <p>No item found</p>;
   return (
     <>
       <h1>List </h1>
-      {message}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map(item =>
           <li className="list-group-item" key={item}>{item}</li>)}

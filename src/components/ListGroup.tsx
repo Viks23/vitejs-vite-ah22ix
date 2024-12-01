9f7714bf754da35ebf926e470eb2b167ca0e46fd
Viks23 <vrai07008@gmail.com>
2024-12-01 19:25:11 +0530

    V1.0.0.0.8

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -12,8 +12,8 @@ function ListGroup() {
       <h1>List </h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map(item =>
-          <li className="list-group-item" key={item} onClick={() => console.log({ item })}>{item}</li>)}
+        {items.map((item, index) =>
+          <li className="list-group-item" key={item} onClick={() => console.log(item, index)}>{item}</li>)}
       </ul>
     </>
   );

f1dc6bdfea14a9343a17093bf3ecc806efaa8337
Viks23 <vrai07008@gmail.com>
2024-12-02 14:26:16 +0530

    V1.0.0.1.4

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
 
   return (
     <div>
-      <Alert> Hello <b>World</b></Alert>
+      <Button></Button>
     </div>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+const Button = () => {
+  return (
+    <div className='btn btn-primary'>Button</div>
+  )
+}
+
+export default Button;
